e7aaf7d2b0e81fd7e1c38aa68293e4463e7affe0
Code-Knight-Debjit <codeknightdebjit@gmail.com>
2026-07-23 01:41:51 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
+      'https://kiaspire.debjit-paul.tech',
       "http://127.0.0.1:5500",
       "http://localhost:5500",
       "http://localhost:3000"
